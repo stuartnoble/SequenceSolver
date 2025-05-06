b0fbca6f0244b09bf88323423020498536388c8b
Stuart Noble <github@stuartnoble.com>
2025-05-06 11:59:16 +0100

    Adding missing commit from laption

--- SequenceSolver/Program.fs
+++ SequenceSolver/Program.fs
@@ -1,6 +1,16 @@
-﻿let rec seq(n:int): int =
+// A recursive function to solve the problem:
+
+// Calculate a sequence of integers that is monotonically increasing, and never decreases.
+// The first value in the sequence is 1, so when n = 1, f(n) = 1.
+// The term for (n + 1) is expressed by f(n + 1) = 1 + f(n + 1 - f(f(n))).
+// The maximum input value for n is 9999 
+// Example:
+// Input: n = 5
+/// Output: 3
+ 
+ let rec seq(n:int): int =
     match n with
-    | 0 | 1 -> n
-    | n -> 1 + seq(n - seq(seq(n - 1)))
+    | 1 -> 1
+    | _ -> 1 + seq(n - seq(seq(n - 1)))
 
 let run = seq(5) |> printfn "%i"
